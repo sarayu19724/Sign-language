b1187162ebf612665df92b70180d0a251c168641
sarayu19724 <madigundusarayu@gmail.com>
2025-02-16 01:29:43 +0530

    ..

--- sign detection model/preprocess.py
+++ sign detection model/preprocess.py
@@ -5,7 +5,7 @@ import mediapipe as mp
 # Initialize MediaPipe Hands
 mp_hands = mp.solutions.hands
 mp_drawing = mp.solutions.drawing_utils
-
+mp_drawing_styles = mp.solutions.drawing_styles
 # Set up MediaPipe Hands
 hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.5)
 
@@ -43,7 +43,8 @@ for subdir, _, files in os.walk(input_folder):
         # Draw landmarks on the image
         if results.multi_hand_landmarks:
             for hand_landmarks in results.multi_hand_landmarks:
-                mp_drawing.draw_landmarks(image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+                mp_drawing.draw_landmarks(image, hand_landmarks, mp_hands.HAND_CONNECTIONS,mp_drawing_styles.get_default_hand_landmarks_style(),
+                mp_drawing_styles.get_default_hand_connections_style())
 
         # Save the processed image with landmarks
         output_image_path = os.path.join(current_output_folder, f"landmark_{image_name}")
